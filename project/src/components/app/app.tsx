eb4ea6abfe0c4f29fed265b2c78772c070cf42f2
Grit <perepadin00@gmail.com>
2021-12-30 02:08:37 +0300

    Create Pages and Route

---
+++ project/src/components/app/app.tsx
@@ -0,0 +1,42 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import MainPage from '../main-page/main-page';
+import LoginScreen from '../login-screen/login-screen';
+import FavoritesScreen from '../favorites-screen/favorites-screen';
+import PropertyScreenNotLogged from '../property-screen-not-logged/property-screen-not-logged';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import PrivateRoute from '../private-route/private-route';
+import NotFound404 from '../NotFound404/NotFound404';
+
+const Setting = {
+  PLACE_CARD_COUNT: 5,
+};
+
+function App(): JSX.Element {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path={AppRoute.Main} exact>
+          <MainPage placeCardCount={Setting.PLACE_CARD_COUNT} />
+        </Route>
+        <Route path={AppRoute.Login} exact component={LoginScreen} />
+        <PrivateRoute
+          exact
+          path={AppRoute.Favorites}
+          render={() => <FavoritesScreen />}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        >
+        </PrivateRoute>
+        <Route path={AppRoute.Offer} exact>
+          <PropertyScreenNotLogged />
+        </Route>
+        <Route
+          render={(props) => (
+            <NotFound404 />
+          )}
+        />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
